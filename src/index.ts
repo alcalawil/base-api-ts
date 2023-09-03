2bd4b7d4beabcca23d8c62be43b17ed52c9198a4
Wilfredo Alcala <alcalawilfre@gmail.com>
2023-09-03 12:23:58 -0400

    hagase la luz

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { exit } from 'node:process';
+import { setupServer } from './server';
+import { createModules } from './modules';
+import { connectDb } from './shared/mongo';
+import { config } from './shared/config';
+
+async function main() {
+	try {
+		console.log('Starting server...');
+		await connectDb();
+		console.log('Database connected');
+
+		const modules = createModules(config);
+
+		setupServer(config, modules);
+	} catch (err) {
+		console.error('RED CODE -- ', err);
+		exit(1);
+	}
+}
+
+main();
